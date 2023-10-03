7cbd3c25ae09115fc52f0b6c890e6b8bc1ebc277
Oliver Köchli <oli.koechli@outlook.com>
2023-10-03 21:44:19 +0200

    changes in timerange baseline

--- baselines/TimeRangeRegressor.py
+++ baselines/TimeRangeRegressor.py
@@ -32,18 +32,30 @@ class TimeRangeRegressor:
     needed_rows = x[(x['time'] >= str(self.wake_up_range[0].time())) & (x['time'] <= str(self.wake_up_range[-1].time()))]
     needed_rows2 = x[(x['time'] >= str(self.sleep_range[0].time())) & (x['time'] <= str(self.sleep_range[-1].time()))]
     df = pd.concat([needed_rows, needed_rows2], axis=0)
-
-    current_wake_up = random.choice(self.wake_up_range)
-    current_sleep = random.choice(self.sleep_range)
+    first_date = df.iloc[0]
+    
+    current_wake_up = first_date['timestamp'][0:10] + 'T' + str(random.choice(self.wake_up_range))[11:19]
+    current_sleep = first_date['timestamp'][0:10] + 'T' + str(random.choice(self.sleep_range))[11:19]
     return_values = []
+    last_series_id = first_date['series_id']
     with tqdm.tqdm(total=df.shape[0]) as p_bar:
       for index, row in df.iterrows():
-          if row['time'] == str(current_wake_up.time()):
+          
+          if str(row['timestamp'])[:19] == current_wake_up:
               return_values.append({'row_id': len(return_values), 'series_id': row['series_id'], 'step': row['step'], 'event': 'wakeup', 'score': 0.5, 'timestamp': row['timestamp']})
-              current_wake_up = random.choice(self.wake_up_range)
-          elif row['time'] == str(current_sleep.time()):
+              datetime_object = datetime.strptime(row['timestamp'][:10], '%Y-%m-%d') + timedelta(days=1)
+              current_wake_up = str(datetime_object.date()) + 'T' + str(random.choice(self.wake_up_range))[11:19]
+              
+          elif str(row['timestamp'])[:19] == current_sleep:
               return_values.append({'row_id': len(return_values), 'series_id': row['series_id'], 'step': row['step'], 'event': 'onset', 'score': 0.5, 'timestamp': row['timestamp']})
-              current_sleep = random.choice(self.sleep_range)
+              datetime_object = datetime.strptime(row['timestamp'][:10], '%Y-%m-%d') + timedelta(days=1)
+              current_sleep = str(datetime_object.date()) + 'T' + str(random.choice(self.sleep_range))[11:19]
 
+          if last_series_id != row['series_id']:
+            datetime_object = datetime.strptime(row['timestamp'][:10], '%Y-%m-%d')
+            current_sleep = str(datetime_object.date()) + 'T' + str(random.choice(self.sleep_range))[11:19]
+            current_wake_up = str(datetime_object.date()) + 'T' + str(random.choice(self.wake_up_range))[11:19]
+            last_series_id = row['series_id']
+             
           p_bar.update(1)
     return pd.DataFrame(return_values)
