67ea3c1db27b45a1ba1d7fa3969ff80ec17a1d39
Oliver Köchli <oli.koechli@outlook.com>
2023-10-27 08:33:21 +0200

    started rnn

---
+++ models/rnn.py
@@ -0,0 +1,105 @@
+import lightning as L
+import torch
+import torch.nn.functional as F
+from torch.optim import AdamW
+from torch.optim.lr_scheduler import LinearLR
+from torchmetrics import MetricCollection
+from torchmetrics.classification import MulticlassAccuracy, MulticlassPrecision, MulticlassRecall, MulticlassF1Score
+
+
+class PyTorchRNN(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_hidden_layers, num_classes):
+        super(PyTorchRNN, self).__init__()
+        
+        # Number of hidden dimensions
+        self.hidden_dim = hidden_dim
+        
+        # Number of hidden layers
+        self.layer_dim = num_hidden_layers
+        
+        # RNN
+        self.rnn = torch.nn.RNN(input_dim, hidden_dim, num_hidden_layers, batch_first=True, nonlinearity='relu')
+        
+        # Readout layer
+        self.fc = torch.nn.Linear(hidden_dim, num_classes)
+    
+    def forward(self, x):
+        
+        # Initialize hidden state with zeros
+        h0 = Variable(torch.zeros(self.layer_dim, x.size(0), self.hidden_dim))
+            
+        # One time step
+        out, hn = self.rnn(x, h0)
+        out = self.fc(out[:, -1, :]) 
+        return out
+
+
+class LightningModel(L.LightningModule):
+
+    def __init__(self, model=None, hidden_dim=None, num_hidden_layers=None, learning_rate=None):
+        super().__init__()
+
+        self.save_hyperparameters()
+
+        self.num_features = 14
+        self.num_classes = 2
+        self.hidden_dim = hidden_dim
+        self.num_hidden_layers = num_hidden_layers
+        self.learning_rate = learning_rate
+
+        # model
+        if model is None:
+            self.model = PyTorchRNN(self.num_features, self.hidden_dim, self.num_hidden_layers, self.num_classes)
+
+        # metrics
+        metrics = MetricCollection([
+            MulticlassAccuracy(num_classes=self.num_classes),
+            MulticlassPrecision(num_classes=self.num_classes),
+            MulticlassRecall(num_classes=self.num_classes),
+            MulticlassF1Score(num_classes=self.num_classes)
+        ])
+        self.train_metrics = metrics.clone(prefix='train_')
+        self.val_metrics = metrics.clone(prefix='val_')
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_nb):
+        loss, true_labels, logits = self._shared_step(batch)
+
+        self.log('train_loss', loss)
+        self.train_metrics(logits, true_labels)
+        self.log_dict(self.train_metrics, on_epoch=True, on_step=False)
+
+        return loss
+
+    def validation_step(self, batch, batch_nb):
+        loss, true_labels, logits = self._shared_step(batch)
+
+        self.log('val_loss', loss)
+        self.val_metrics(logits, true_labels)
+        self.log_dict(self.val_metrics)
+
+        return loss
+
+    def _shared_step(self, batch):
+        features, true_labels = batch
+        logits = self(features)
+
+        loss = F.cross_entropy(logits, true_labels)
+        return loss, true_labels, logits
+
+    def configure_optimizers(self):
+        optimizer = AdamW(self.parameters(), lr=self.hparams.learning_rate)
+
+        scheduler = LinearLR(optimizer, start_factor=1.0, end_factor=0.001, total_iters=self._num_steps())
+        scheduler = {"scheduler": scheduler, "interval": "step", "frequency": 1}
+
+        return [optimizer], [scheduler]
+
+    def _num_steps(self) -> int:
+        """Get number of steps"""
+        train_dataloader = self.trainer.datamodule.train_dataloader()
+        dataset_size = len(train_dataloader.dataset)
+        num_steps = dataset_size * self.trainer.max_epochs // self.trainer.datamodule.batch_size
+        return num_steps
---
+++ models/rnn_training_cli.py
@@ -0,0 +1,62 @@
+import sys
+import json
+import wandb
+
+from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint, EarlyStopping
+from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch.cli import LightningCLI
+from lightning.pytorch import Trainer
+from watermark import watermark
+
+from data import CustomDataModule
+from rnn import LightningModel, PyTorchRNN
+
+if __name__ == "__main__":
+
+    print(watermark(packages="torch,lightning"))
+
+    print(f"The provided arguments are {sys.argv[1:]}", end="\n\n")
+
+    # Params
+    model_name = "RNN"
+    batch_size = 500
+
+    # Logger
+    with open("../config/config.json", "r") as jsonfile:
+        data = json.load(jsonfile)
+        subscription_key = data["wandb"]["subscription_key"]
+    wandb.login(key=subscription_key)
+    wandb_logger = WandbLogger(project="aich", log_model=True)
+
+    # Callbacks
+    checkpoint_callback = ModelCheckpoint(
+        save_top_k=1,
+        verbose=True,
+        monitor="val_loss",
+        mode="min"
+    )
+    early_stopping_callback = EarlyStopping(mode="min", monitor="val_loss", patience=1)
+    lr_monitor = LearningRateMonitor(logging_interval="step")
+
+    #cli = LightningCLI(
+    #    model_class=LightningModel,
+    #    datamodule_class=CustomDataModule,
+    #    run=False,
+    #    save_config_kwargs={"overwrite": True},
+    #    seed_everything_default=42,
+    #    trainer_defaults={
+    #        "callbacks": [checkpoint_callback, early_stopping_callback, lr_monitor],
+    #        "logger": wandb_logger,
+    #        "max_epochs": 5
+    #    },
+    #)
+
+    rnnModel = LightningModel(num_hidden_layers=14, hidden_dim=100, learning_rate=0.005)
+    #cli.trainer.fit(rnnModel, CustomDataModule())
+    trainer = Trainer(
+        logger=wandb_logger,
+        max_epochs=5,
+        callbacks=[checkpoint_callback]
+    )
+
+    trainer.fit(rnnModel, datamodule=CustomDataModule())
